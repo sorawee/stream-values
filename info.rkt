2e5a84ffd177b3c781c09be409932e2ca126681c
Sorawee Porncharoenwase <sorawee.pwase@gmail.com>
2020-04-20 18:00:45 -0700

    Change pkg-desc

--- info.rkt
+++ info.rkt
@@ -3,6 +3,6 @@
 (define deps '("base"))
 (define build-deps '("scribble-lib" "racket-doc" "rackunit-lib"))
 (define scribblings '(("scribblings/stream-values.scrbl" ())))
-(define pkg-desc "A library to manipulate multiple values in streams")
+(define pkg-desc "A library that manipulates multiple values in streams")
 (define version "0.0")
 (define pkg-authors '(sorawee))
